f3b382714375cfda742ec0d1f494435fedd24484
Jeff Davis <jeff@loudifier.com>
2025-05-02 23:31:24 -0700

    manually add 'use typo metrics' bit to converted ufo sources

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -4,4 +4,7 @@ cd "$(dirname "$0")"
 for sfd in ../sources/*.sfd;
 do
     fontforge -c 'open(argv[1]).generate(argv[2])' $sfd ${sfd%.*}.ufo
+
+    # fontforge bug #5450 does not properly export 'use typo metrics' bit
+    xmlstarlet ed --inplace -s '//plist/dict[last()]' -t elem -n "key" -v "openTypeOS2Selection" -s '//plist/dict[last()]' -t elem -n "array" -s '//plist/dict[last()]/array[last()]' -t elem -n "integer" -v 7 ${sfd%.*}.ufo/fontinfo.plist
 done
