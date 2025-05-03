020cbd155f79ec99c04f9ffd2958ac39094cb922
Jeff Davis <jeff@loudifier.com>
2025-05-03 01:03:15 -0700

    strip extra tags from ufo files that cause fontbakery failures

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -7,4 +7,10 @@ do
 
     # fontforge bug #5450 does not properly export 'use typo metrics' bit
     xmlstarlet ed --inplace -s '//plist/dict[last()]' -t elem -n "key" -v "openTypeOS2Selection" -s '//plist/dict[last()]' -t elem -n "array" -s '//plist/dict[last()]/array[last()]' -t elem -n "integer" -v 7 ${sfd%.*}.ufo/fontinfo.plist
+
+    # fontforge exports tags that are interpreted weird by fontbakery/Google Fonts. Just delete them
+    xmlstarlet ed --inplace -d '//plist/dict/key[contains(text(),"styleMapFamilyName")]/following-sibling::string[1]' ${sfd%.*}.ufo/fontinfo.plist
+    xmlstarlet ed --inplace -d '//plist/dict/key[contains(text(),"styleMapFamilyName")]' ${sfd%.*}.ufo/fontinfo.plist
+    xmlstarlet ed --inplace -d '//plist/dict/key[contains(text(),"styleMapStyleName")]/following-sibling::string[1]' ${sfd%.*}.ufo/fontinfo.plist
+    xmlstarlet ed --inplace -d '//plist/dict/key[contains(text(),"styleMapStyleName")]' ${sfd%.*}.ufo/fontinfo.plist
 done
